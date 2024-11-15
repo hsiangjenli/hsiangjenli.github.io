22b816fba8f5b462e9b93dc1f659d5bab6ac41c6
Hsiang-Jen Li <hsiangjenli@gmail.com>
2024-11-15 21:13:05 +0800

    refactor: Update FontAwesomeIcon enum values and HtmlIcon class
    This commit refactors the FontAwesomeIcon enum values in the `HtmlIcon` class to include the appropriate Font Awesome classes. It also updates the `HtmlIcon` class to use the `fontawesom` attribute instead of the `icon_type` attribute for generating the HTML icon. Additionally, it modifies the `HtmlIcon` class to include the `GITHUB` icon for GitHub URLs. Finally, it adds a sample usage of the `HtmlIcon` class in the `__main__` block.

--- component.py
+++ component.py
@@ -1,39 +1,40 @@
-from pydantic import BaseModel, AnyHttpUrl
 from enum import Enum
+from typing import ClassVar
+
+from pydantic import AnyHttpUrl, BaseModel
+
 
 class FontAwesomeIcon(Enum):
-    HOME = "home"
-    USER = "user"
-    CART = "cart"
-    PDF = "file-pdf"
-    WEB = "globe"
-    MAIL = "envelope"
-    IMAGE = "image"
-    GOOGLE_DRIVE = "google-drive"
-    LINK = "link"
+    PDF = "fa-solid fa-file-pdf"
+    WEB = "fa-solid fa-globe"
+    MAIL = "fa-solid fa-envelope"
+    IMAGE = "fa-solid fa-image"
+    GOOGLE_DRIVE = "fa-brands fa-google-drive"
+    GITHUB = "fa-brands fa-github"
+    DEFAULT = "fa-solid fa-link"
+
 
 class HtmlIcon(BaseModel):
     text: str
-    fontawesom: str
+    fontawesom: str = None
     href: AnyHttpUrl
 
-    EXTENSION_MAPPING = {
+    EXTENSION_MAPPING: ClassVar[dict[tuple[str, ...], FontAwesomeIcon]] = {
         (".pdf",): FontAwesomeIcon.PDF,
         (".png", ".jpg", ".jpeg", ".gif", ".bmp"): FontAwesomeIcon.IMAGE,
     }
 
-    DOMAIN_MAPPING = {
+    DOMAIN_MAPPING: ClassVar[dict[str, FontAwesomeIcon]] = {
         "drive.google.com": FontAwesomeIcon.GOOGLE_DRIVE,
+        "github.com": FontAwesomeIcon.GITHUB,
     }
 
     @property
     def icon_type(self) -> FontAwesomeIcon:
-
         if not self.href:
-            return FontAwesomeIcon.UNKNOWN
+            return FontAwesomeIcon.DEFAULT
 
-        url = self.href.lower()
-
+        url = str(self.href).lower()
         for extensions, icon in self.EXTENSION_MAPPING.items():
             if url.endswith(extensions):
                 return icon
@@ -46,10 +47,14 @@ class HtmlIcon(BaseModel):
 
     @property
     def html(self) -> str:
-        if self.fontawesom:
-            icon_html = f'<i class="{self.fontawesom}"></i>'
-        else:
-            icon_html = f'<i class="{self.icon_type.value}"></i>'
+        icon_html = f'<i class="{self.icon_type.value}"></i>'
         if self.href:
-            return f'<a href="{self.href}" target="_blank">{icon_html}</a>'
+            return (
+                f'<a href="{self.href}" target="_blank"> {icon_html} {self.text} </a>'
+            )
         return icon_html
+
+
+if __name__ == "__main__":
+    icon = HtmlIcon(text="PDF", href="https://example.com/document.pdf")
+    print(icon.html)
